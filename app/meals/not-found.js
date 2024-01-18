076e0eb5e5bed3b5d17912556d83ccb4b23ddab0
adjeiessel <essel.adjei-mensah@zeppelin.com>
2024-01-18 19:15:28 +0100

    feat: added a meal specific not-found page from next/navigation. feat: next/navigation function looks for a not found component in the closest not-found page or error page, stop execution and render that

---
+++ app/meals/not-found.js
@@ -0,0 +1,14 @@
+ export default function Notfound() {
+  return (
+    <main className="not-found">
+      <h1>
+        Meal not found
+      </h1>
+      <p> The select meal cannot be found!</p>
+    </main>
+  );
+}
+
+
+
+  
