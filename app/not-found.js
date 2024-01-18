17f732cedbc2adbf798371bf5443680f599ba960
adjeiessel <essel.adjei-mensah@zeppelin.com>
2024-01-18 18:10:57 +0100

    added a not-found component

---
+++ app/not-found.js
@@ -0,0 +1,14 @@
+ export default function Notfound() {
+  return (
+    <main className="not-found">
+      <h1>
+        Page Not Found
+      </h1>
+      <p>Unfortunately, we could not find the requested resource</p>
+    </main>
+  );
+}
+
+
+
+  
